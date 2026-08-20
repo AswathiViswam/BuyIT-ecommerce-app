b709fbafae9db94663443ffea0c19f977ab7306d
Aswathi Viswam <aswathiviswam2110@gmail.com>
2026-08-20 21:42:15 +0530

    added .env to gitignore

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -31,7 +31,7 @@ api.interceptors.response.use(
 
       if (refreshToken) {
         try {
-          const res = await axios.post("http://localhost:5000/api/auth/refresh", {
+          const res = await axios.post(`${import.meta.env.VITE_API_URL}/auth/refresh`, {
             refreshToken,
           });
 
